206a49e5687b0d96b925cb11a0acb1683e53ac9d
Dutervil <dwadson3@gmail.com>
2022-07-04 11:10:35 -0400

    ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'DEVELOPEMENT';
+  title = 'Smart Police | Online Payment ';
 }
